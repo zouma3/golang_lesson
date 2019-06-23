5bec569851a9e0d9138d5ac56d63c5f50f58bc93
zouma3 <the.truth.is.out.there.zoma@gmail.com>
2019-06-23 20:34:40 +0900

    コミット　テスト

--- lesson.go
+++ lesson.go
@@ -24,6 +24,16 @@ func VariableDefinition(){
 		f bool = false
 	)
 	fmt.Println(i,f64,s,t,f)
+
+	i2 := 1
+	f642 := 1.2
+	s2 := "test"
+	t2 := true
+	f2 := false
+	fmt.Println(i2,f642,s2,t2,f2)
+
+	const Pi = 3.14
+	fmt.Println(Pi)
 }
 
 func main(){
